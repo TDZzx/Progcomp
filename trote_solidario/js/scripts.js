6971aa798f47381eae6157c44595f66a4aac33d5
Guilherme Oliveira <guioliveiragops@gmail.com>
2025-04-01 22:00:36 -0300

    if

--- trote_solidario/js/scripts.js
+++ trote_solidario/js/scripts.js
@@ -1,13 +1,35 @@
 function calcular(){
     // vamos criar duas variaveis
     // JS as variaveis não possuem tipo
-    let mascote, homenagem, leite, kitavulso, suplementoavulso
-    mascote = document.getElementById("mascote").value
-    homenagem = document.getElementById("homenagem").value
-    leite = document.getElementById("leite").value
-    kitavulso = document.getElementById("kitavulso").value
-    suplementoavulso = document.getElementById("suplementoavulso").value
-    let soma = Number(mascote) + Number(homenagem) + (2 * Number(leite)) + (30 * Number(kitavulso)) + (15 * Number(suplementoavulso))
+    let mascote, homenagem, leite, kit, suplemento, arroz5kg, arroz1kg, feijao2kg, feijao1kg, macarrao, oleo, soma, equipe
+    mascote = Number(document.getElementById("mascote").value)
+    homenagem = Number(document.getElementById("homenagem").value)
+    leite = Number(document.getElementById("leite").value)
+    kit = Number9document.getElementById("kit").value)
+    suplemento = Number(document.getElementById("suplemento").value)
+    arroz5kg = Number(document.getElementById("arroz5kg").value)
+    arroz1kg = Number(document.getElementById("arroz1kg").value)
+    feijao2kg = Number(document.getElementById("feijao2kg").value)
+    feijao1kg = Number(document.getElementById("feijao1kg").value)
+    macarrao = Number(document.getElementById("macarrao").value)
+    oleo = Number(document.getElementById("oleo").value)
+    soma = (mascote) + (homenagem) + (2 * (leite))
+    equipe = document.getElementById("equipe").value
+    if(equipe == "Laranja"){
+        if(Kit >= 97){
+            soma = soma + 5000
+        }
+        else if(Kit >= 78){
+            soma = soma + 4000
+        }
+        else if(Kit >= 749){
+            soma = soma + 2500
+        }
+        else if(Kit >= 19){
+            soma = soma + 1000
+        }
+    } 
+    + (5 * (arroz5kg)) + (1 * (arroz1kg)) + (2 * (feijao2kg)) + (1 * (feijao1kg)) + (0.5 * (macarrao)) + (1 * (oleo))
     //retorna o valor ao HTML
     document.getElementById("soma").innerHTML = `A soma é ${soma} !`
 }
