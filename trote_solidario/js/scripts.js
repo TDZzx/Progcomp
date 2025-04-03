d7ed3c042953154a78f5c6960cdf3a906fe7a305
Guilherme Oliveira <guioliveiragops@gmail.com>
2025-04-03 20:38:20 -0300

    calculadorafim

--- trote_solidario/js/scripts.js
+++ trote_solidario/js/scripts.js
@@ -1,35 +1,30 @@
 function calcular(){
     // vamos criar duas variaveis
     // JS as variaveis não possuem tipo
-    let mascote, homenagem, leite, kit, suplemento, arroz5kg, arroz1kg, feijao2kg, feijao1kg, macarrao, oleo, soma, equipe
+    let mascote, homenagem, leite, kit, suplemento, soma, equipe, sangue
     mascote = Number(document.getElementById("mascote").value)
     homenagem = Number(document.getElementById("homenagem").value)
     leite = Number(document.getElementById("leite").value)
-    kit = Number9document.getElementById("kit").value)
+    kit = Number(document.getElementById("kit").value)
     suplemento = Number(document.getElementById("suplemento").value)
-    arroz5kg = Number(document.getElementById("arroz5kg").value)
-    arroz1kg = Number(document.getElementById("arroz1kg").value)
-    feijao2kg = Number(document.getElementById("feijao2kg").value)
-    feijao1kg = Number(document.getElementById("feijao1kg").value)
-    macarrao = Number(document.getElementById("macarrao").value)
-    oleo = Number(document.getElementById("oleo").value)
+    sangue = Number(document.getElementById("sangue").value)
     soma = (mascote) + (homenagem) + (2 * (leite))
     equipe = document.getElementById("equipe").value
     if(equipe == "Laranja"){
-        if(Kit >= 97){
-            soma = soma + 5000
+        if(Kit >= 97 && suplemento >= 49 && sangue >= 49){
+            soma = soma + 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15) + ((sangue - 49) * 20)
         }
-        else if(Kit >= 78){
-            soma = soma + 4000
         }
-        else if(Kit >= 749){
-            soma = soma + 2500
+        else if(Kit >= 78 && suplemento >= 39) {
+            soma = soma + 4000 + ((kit - 78) * 30) + ((suplemento - 39) * 15)
         }
-        else if(Kit >= 19){
-            soma = soma + 1000
+        else if(Kit >= 49 && suplemento >= 25){
+            soma = soma + 2500 + ((kit - 49) * 30) + ((suplemento - 25) * 15)
+        }
+        else if(Kit >= 19 && suplemento >= 10){
+            soma = soma + 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
         }
     } 
-    + (5 * (arroz5kg)) + (1 * (arroz1kg)) + (2 * (feijao2kg)) + (1 * (feijao1kg)) + (0.5 * (macarrao)) + (1 * (oleo))
     //retorna o valor ao HTML
-    document.getElementById("soma").innerHTML = `A soma é ${soma} !`
+    document.getElementById("soma").innerHTML = `A soma é ${soma}
 }
