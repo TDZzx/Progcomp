4ce7868f970b77ba09f6c10adc6dfa7cb8f47ace
Guilherme Oliveira <guioliveiragops@gmail.com>
2025-04-08 22:05:36 -0300

    sangue

--- trote_solidario/js/scripts.js
+++ trote_solidario/js/scripts.js
@@ -12,19 +12,41 @@ function calcular(){
     equipe = document.getElementById("equipe").value
     if(equipe == "Laranja"){
         if(Kit >= 97 && suplemento >= 49 && sangue >= 49){
-            soma = soma + 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15) + ((sangue - 49) * 20)
-        }
+            soma = soma + 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15) 
         }
         else if(Kit >= 78 && suplemento >= 39) {
-            soma = soma + 4000 + ((kit - 78) * 30) + ((suplemento - 39) * 15)
+            soma = soma + 4000 + ((kit - 78) * 30) + ((suplemento - 39) * 15) 
         }
         else if(Kit >= 49 && suplemento >= 25){
-            soma = soma + 2500 + ((kit - 49) * 30) + ((suplemento - 25) * 15)
+            soma = soma + 2500 + ((kit - 49) * 30) + ((suplemento - 25) * 15) 
         }
         else if(Kit >= 19 && suplemento >= 10){
-            soma = soma + 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
+            soma = soma + 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15) 
+        }
+        if(sangue >= 49){
+            soma = soma + 2500 + ((sangue - 49) * 20)
+        }
+        else{
+            soma = soma + (sangue * 20)
         }
     } 
+    else if(equipe == "Preta"){
+
+        if(sangue >= 52){
+            soma = soma + 2500 + ((sangue - 49) * 20)
+        }
+        else{
+            soma = soma + (sangue * 20)
+        }
+
+    }
+    else if(equipe == "Roxa"){
+    }
+        else if(equipe == "Verde"){
+        }
+            else{
+
+            }
     //retorna o valor ao HTML
-    document.getElementById("soma").innerHTML = `A soma é ${soma}
+    document.getElementById("soma").innerHTML = `A soma é ${soma}`
 }
